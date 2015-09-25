affd4de6ebf0b4ccbbdde1d0f997874be4c1698d
徐基伟 <1072838309@qq.com>
2015-09-25 17:47:09 +0800

    reset master

--- test/available.test.js
+++
@@ -1,5 +0,0 @@
-var test = require('test-on-fire');
-var AvailabilityTest = test.AvailabilityTest;
-var schema = JSON.parse(require('fs').readFileSync('./validator.config.json', {encoding: 'utf8'}));
-var test = new AvailabilityTest(schema, {urlPrefix: 'http://localhost:3000'});
-test.run();
